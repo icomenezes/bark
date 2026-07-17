63fb76918bc18c587dcdd8fef4fee818fa9fd834
icomenezes <icomenezes@hotmail.com>
2026-07-17 16:14:06 -0300

    feat: remove autoexclusao de conta do perfil do cliente

--- routes/web.php
+++ routes/web.php
@@ -72,7 +72,6 @@ Route::post('/api/register', [PublicRegisterController::class, 'store'])
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 // Envelopes (assinatura eletrônica multi-signatário)
